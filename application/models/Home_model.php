27dccc9b0c7ef673616855d5d4691683cbecd2cd
Rohit Jadhav <rohitjadhav@Rohits-MacBook-Air.local>
2020-05-25 13:59:26 +0530

    add the customerId or emailid and password check query

--- application/models/Home_model.php
+++ application/models/Home_model.php
@@ -19,8 +19,10 @@ class Home_model extends CI_Model{
   }
 
   public function check_user($name,$password){
-    $this->db->where('u_emailId',$name);
-    $this->db->where('u_password',$password);
+    $where = "u_password='$password' AND (u_customerId='$name' OR u_emailId='$name')";
+    $this->db->where($where);
+    // $this->db->or_where('u_customerId',$name);
+    // $this->db->where('u_password',$password);
 
     $sql = $this->db->get('s_user');
     //exit;
